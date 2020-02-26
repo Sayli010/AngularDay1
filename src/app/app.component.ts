50db849f32fcd26eaaccf4c66dbd1347db97c81c
Minskoleopc <minskoleopc@gmail.com>
2020-02-26 21:59:06 +0530

    Day1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Day1';
+  title =  'This is two way data binding';
+  heading = "This is string interpollation";
+  isDisabled = true;
+
+  onSave(){
+    // this.isDisabled  = !this.isDisabled;
+    this.isDisabled == true ? this.isDisabled = false : this.isDisabled =  true;
+  }
 }
